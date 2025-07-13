18bd0c9d82897f03ed88a0fa32d38981a03b11de
karan chaudhary <124464054+Karanchaudhary121@users.noreply.github.com>
2025-07-13 22:50:10 +0530

    goahed

---
+++ dice_game_p3/src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components" 
+
+const Button = styled.button`
+ `;
+
+function App() {
+  return ( 
+    <>
+      <Button> this is styled component button </Button>
+    </>
+  )
+}
+
+export default App
